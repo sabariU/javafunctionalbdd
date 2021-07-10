cfdfdd38034288777289fc1ab4ebeba916799d11
SabariU <domail2sab07@gmail.com>
2021-07-10 20:27:13 +0800

    sync

--- src/main/java/com/functional/pgming/java/warmup/funcexercisebasic/Ex1_Consumer.java
+++ src/main/java/com/functional/pgming/java/warmup/funcexercisebasic/Ex1_Consumer.java
@@ -1,2 +1,40 @@
-package com.functional.pgming.java.warmup.funcexercisebasic.model;public class Ex1_Consumer {
+package com.functional.pgming.java.warmup.funcexercisebasic;
+
+import java.util.function.Consumer;
+
+public class Ex1_Consumer {
+
+
+    private String name = "dfsdfsdfsd" ;
+
+
+    public Consumer<String> printStringConsumerV5 = s -> System.out.println(s);
+
+    public Consumer<String> printStringConsumerV4 = (s) -> {
+        System.out.println(s);
+    };
+
+
+    public Consumer<String> printStringConsumerV3 = (String s) -> {
+            System.out.println(s);
+    };
+
+    public Consumer printStringConsumerV2 =  new Consumer<String>(){
+
+        @Override
+        public void accept(String s) {
+            System.out.println(s);
+        }
+    };
+
+    public class PrintStringConsumerV1 implements
+            Consumer<String> {
+
+        @Override
+        public void accept(String s) {
+            System.out.println(s);
+        }
+    }
+
+
 }
--- src/test/java/com/functional/pgming/java/warmup/funcexercisebasic/Ex1_ConsumerTest.java
+++ src/test/java/com/functional/pgming/java/warmup/funcexercisebasic/Ex1_ConsumerTest.java
@@ -1,4 +1,62 @@
+package com.functional.pgming.java.warmup.funcexercisebasic;
+
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.function.Consumer;
+
+
+@RunWith(JUnit4.class)
 public class Ex1_ConsumerTest extends TestCase {
-  
+
+    Ex1_Consumer ex1_consumer;
+
+    @Before
+    public void init() {
+        ex1_consumer =
+                new Ex1_Consumer();
+    }
+
+    @Test
+    public void testPrintConsumerV5(){
+        Consumer printStringConsumerV5 = ex1_consumer.printStringConsumerV5;
+        printStringConsumerV5.accept("Got a decent lambda expression");
+    }
+
+    @Test
+    public void testPrintConsumerV4(){
+        Consumer printStringConsumerV4 = ex1_consumer.printStringConsumerV4;
+        printStringConsumerV4.accept("Using a variant with lambda operator without defining argument type");
+    }
+
+    @Test
+    public void testPrintConsumerV3(){
+        Consumer printStringConsumerV3 = ex1_consumer.printStringConsumerV3;
+        printStringConsumerV3.accept("Using a varint with lambda operator");
+    }
+
+    @Test
+    public void testPrintConsumerV2(){
+        Consumer printStringConsumerV2 = ex1_consumer.printStringConsumerV2;
+        printStringConsumerV2.accept("Invoking the second variant of Consumer implementation");
+    }
+
+    @Test
+    public void testPrintConsumerV1(){
+        Ex1_Consumer.PrintStringConsumerV1 printStringConsumerV1 =
+            ex1_consumer.new PrintStringConsumerV1();
+
+        Consumer printStringConsumerV1AnotherVariant =
+                ex1_consumer.new PrintStringConsumerV1();
+
+        printStringConsumerV1.accept("Hello world!");
+
+    }
+
+
+
+
 }
