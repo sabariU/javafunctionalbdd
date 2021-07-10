18ca6515b5d5223c228d6469f715ea7a367149a2
Venkata Raghu <venkataraghu.g@adappt.ai>
2021-07-10 19:08:50 +0530

    second one

--- src/main/java/com/functional/pgming/java/warmup/funcexercisebasic/model/Ex1_Consumer.java
+++ src/main/java/com/functional/pgming/java/warmup/funcexercisebasic/model/Ex1_Consumer.java
@@ -1,2 +1,26 @@
-package com.functional.pgming.java.warmup.funcexercisebasic.model;public class Ex1_Consumer {
+package com.functional.pgming.java.warmup.funcexercisebasic.model;
+
+import java.util.function.Consumer;
+
+public class Ex1_Consumer {
+
+
+    public Consumer printStringConsumerV2 =  new Consumer<String>(){
+
+        @Override
+        public void accept(String s) {
+            System.out.println(s);
+        }
+    };
+
+    public class PrintStringConsumerV1 implements
+            Consumer<String> {
+
+        @Override
+        public void accept(String s) {
+            System.out.println(s);
+        }
+    }
+
+
 }
--- src/test/java/com/functional/pgming/java/warmup/funcexercisebasic/model/Ex1_ConsumerTest.java
+++ src/test/java/com/functional/pgming/java/warmup/funcexercisebasic/model/Ex1_ConsumerTest.java
@@ -1,4 +1,45 @@
+package com.functional.pgming.java.warmup.funcexercisebasic.model;
+
+import com.functional.pgming.java.warmup.funcexercisebasic.model.Ex1_Consumer;
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.function.Consumer;
+
+
+@RunWith(JUnit4.class)
 public class Ex1_ConsumerTest extends TestCase {
-  
+
+    Ex1_Consumer ex1_consumer;
+
+    @Before
+    public void init() {
+        ex1_consumer =
+                new Ex1_Consumer();
+    }
+
+    @Test
+    public void testPrintConsumerV2(){
+        Consumer printStringConsumerV2 = ex1_consumer.printStringConsumerV2;
+        printStringConsumerV2.accept("the second variant of Consumer");
+    }
+
+    @Test
+    public void testPrintConsumerV1(){
+        Ex1_Consumer.PrintStringConsumerV1 printStringConsumerV1 =
+                ex1_consumer.new PrintStringConsumerV1();
+
+        Consumer printStringConsumerV1AnotherVariant =
+                ex1_consumer.new PrintStringConsumerV1();
+
+        printStringConsumerV1.accept("Hello world!");
+
+    }
+
+
+
+
 }
